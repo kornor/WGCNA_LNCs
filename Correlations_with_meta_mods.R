49b2a7231fc89bd760cee3bfc88a2e5e1672fb22
Korinne Northwood <kozimoto@gmail.com>
2015-07-28 10:22:06 +1000

    Oh yes, finally moved and fixed all of this

--- Correlations_with_meta_mods.R
+++ Correlations_with_meta_mods.R
@@ -1,18 +1,40 @@
 
+###### THIS IS TO COMPARE THE LNC MODS TO THE EXP MODS
+### NEED TO DO FOR BOTH TCGA AND META AND THEN PASTE INTO
+### THE TRAITS HEATMAPS FILES
+################## LET's DO THIS like BRUTUS!
+
+setwd("~/Bioinformatics Work/Meth & RNA/WGCNA_LNC_ONLY")
+
+## Load libraries etc
+
+library(WGCNA)
+library(plyr)
+library(ggplot2)
+library(gplots)
+
+## load in the the lncs files
+
+load(file= "lncRNA_trimmed_input.RData")
+load(file = "Dynamic_lncRNA.RData")
+
+
 ### load in the exp file of the modules in all gene set
 load( file = "Modules_DS0.RData")
 load(file = "MetaAnalysis_trimmed_input.RData")
-### ME1A for the match
+
+
+### ME1A for the match (start with TCGA)
 
 rownames(ME_1A) <- colnames(datExpr1)
+  list1 <- intersect(rownames(ME_1A), rownames(datExpr))
+    ME <- ME_1A[list1,]
+      datExpr <- datExpr[list1,]
 
-list1 <- intersect(rownames(ME_1A), rownames(datExpr))
+other_mod <- ME  # this creates another "traits" file for ExpMods
 
-ME <- ME_1A[list1,]
-datExpr <- datExpr[list1,]
-
-other_mod <- ME
-
+####### remember that datExpr is now the lncs exp file
+### modules1 is the mRNA exp file
 
 nGenes = ncol(datExpr);
 nSamples = nrow(datExpr);
@@ -24,14 +46,9 @@ MEs = orderMEs(MEs0)
 moduleTraitCor = cor(MEs,other_mod, use = "p");
 moduleTraitPvalue = corPvalueStudent(moduleTraitCor, nSamples);
 
+### just have a quick look at it
 sizeGrWindow(10,6)
-# Will display correlations and their p-values
-textMatrix = paste(signif(moduleTraitCor, 2), "\n(",
-                   signif(moduleTraitPvalue, 1), ")", sep = "");
 
-##########Will only display correlations
-textMatrix = paste(signif(moduleTraitCor, 2),sep = "");
-dim(textMatrix) = dim(moduleTraitCor)
 par(mar = c(6, 8.5, 3, 3));
 
 # Display the values within a heatmap plot
@@ -41,7 +58,7 @@ labeledHeatmap(Matrix = moduleTraitCor,
                ySymbols = names(MEs),
                colorLabels = FALSE,
                colors = blueWhiteRed(50),
-               textMatrix = textMatrix,
+               #textMatrix = textMatrix,
                setStdMargins = FALSE,
                cex.text = 0.85,
                zlim = c(-1,1),
@@ -50,3 +67,10 @@ labeledHeatmap(Matrix = moduleTraitCor,
 
 
 #### write out this correlation matrix for heatmaps later
+
+write.table(moduleTraitCor, "LNCmods_v_TCGAmods.txt", sep = "\t")
+write.table(moduleTraitPvalue, "LNCmods_TCGAmods_pValue.txt", sep = "\t")
+
+#####################################
+### Ah yeah crap can't match the META because the samples aren't the same 
+### workaround???
